ee4b373d881b35041e4107ee88ea37c0bd494ea4
Elaidzha1940 <eli080293@gmail.com>
2024-03-12 20:48:38 +0300

    VIPER

---
+++ VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: VIPER
+//  File: ViewController.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.03.2024
+//
+//  */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+    }
+}
+
